7e839c061efc3cdec56757244f14810a37d043bb
Helmi Ben Zina <123458472+helmibenzina101@users.noreply.github.com>
2024-11-29 16:55:18 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any''.*');
 
 Route::get('/contact', function () {
     return view('contact');     
